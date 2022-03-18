84e8e58bd87daa3d2abf5e18cb3db6019449ba49
skarltjr <kisa0828@naver.com>
2022-03-18 14:28:20 +0900

    fix : user-service 반환 타입 및 작은 수정

--- user-service/src/main/java/com/msa/domain/commands/handler/UserCommandHandler.java
+++ user-service/src/main/java/com/msa/domain/commands/handler/UserCommandHandler.java
@@ -4,11 +4,14 @@ import com.msa.domain.UserRepository;
 import com.msa.domain.commands.CreateUserCommand;
 import com.msa.domain.commands.DeleteUserCommand;
 import com.msa.domain.model.User;
+import com.msa.domain.model.UserRole;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class UserCommandHandler {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
 
     public User createUser(CreateUserCommand command) {
@@ -16,6 +19,7 @@ public class UserCommandHandler {
                 .email(command.getEmail())
                 .username(command.getUserName())
                 .password(command.getPassword())
+                .role(UserRole.ROLE_USER)
                 .build();
         return userRepository.save(user);
     }
--- user-service/src/main/java/com/msa/presentation/controller/UserController.java
+++ user-service/src/main/java/com/msa/presentation/controller/UserController.java
@@ -4,25 +4,25 @@ import com.msa.application.UserFacade;
 import com.msa.application.request.DeleteUserRequest;
 import com.msa.application.request.SignUpRequest;
 import com.msa.domain.model.User;
-import com.msa.reponse.CommonResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users/")
 public class UserController {
-    private UserFacade userFacade;
+    private final UserFacade userFacade;
 
     @PostMapping("signUp")
-    private CommonResponse singUp(SignUpRequest request) {
+    private ResponseEntity singUp(@RequestBody SignUpRequest request) {
         User user = userFacade.signUp(request);
-        return new CommonResponse(user);
+        return ResponseEntity.ok().body(user);
     }
 
     @DeleteMapping()
-    private CommonResponse deleteUser(DeleteUserRequest request) {
+    private ResponseEntity deleteUser(DeleteUserRequest request) {
         userFacade.deleteUser(request);
-        return new CommonResponse(request.getUserId());
+        return ResponseEntity.ok().body(request.getUserId());
     }
 }
