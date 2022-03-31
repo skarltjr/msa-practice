9be81e2269d15c0275f30192693cc23178bca71f
skarltjr <kisa0828@naver.com>
2022-04-01 01:43:49 +0900

    feat : prodcuer & consumer 메세지 통신에 필요한 객체 생성

---
+++ user-service/src/main/java/com/msa/infrastructure/kafka/message/ArticleCreateMessage.java
@@ -0,0 +1,17 @@
+package com.msa.infrastructure.kafka.message;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ArticleCreateMessage {
+    private Long articleId;
+    private Long writerId;
+
+    public ArticleCreateMessage(Long articleId, Long writerId) {
+        this.articleId = articleId;
+        this.writerId = writerId;
+    }
+}
